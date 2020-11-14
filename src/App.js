b350173b0050503fc7bacd44d3e8e9aeeeff5c17
Pachciar <pachciar3@gmail.com>
2020-11-14 17:40:42 +0100

    Excersize 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const defaultTIP = 10;
+  const VAT = 23;
+  const [netto, setNetto] = useState('');
+  const [tip, setTip] = useState(defaultTIP);
+  const [result, setResult] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const result = ((tip / 100 * netto) + netto) * (1 + (VAT / 100));
+    setResult(result);
+  }
+
+  function handleChange(e) {
+    if (e.target.name === "netto") {
+      setNetto(parseInt(e.target.value));
+    } else {
+      setTip(parseInt(e.target.value));
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {result ?
+        <div>{result}</div>
+        :
+        <form onSubmit={handleSubmit}>
+          <input type="number" onChange={handleChange} name="netto" />
+          <select onChange={handleChange} name="tip" id="" defaultValue={defaultTIP}>
+            <option value="0">0%</option>
+            <option value="5">5%</option>
+            <option value="10">10%</option>
+            <option value="15">15%</option>
+            <option value="20">20%</option>
+          </select>
+          <button type="submit">Przelicz</button>
+        </form>
+      }
     </div>
   );
 }
